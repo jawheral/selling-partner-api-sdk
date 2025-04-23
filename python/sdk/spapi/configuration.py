272c4137cee4e1c2bd301653de2114c599e8fc3e
Jawher Allani <allajaw@amazon.de>
2025-04-23 16:09:31 +0200

    [Python SDK] use {{last}} instead of {{hasMore}} and other improvements

--- python/sdk/spapi/configuration.py
+++ python/sdk/spapi/configuration.py
@@ -242,6 +242,5 @@ class Configuration(object):
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: v1\n"\
                "SDK Package Version: 1.0.0".\
                format(env=sys.platform, pyversion=sys.version)
