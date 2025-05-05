628101eef8ba20c82276f25f4c3f6c9499762cfb
jawheral <173008950+jawheral@users.noreply.github.com>
2025-05-05 11:04:23 +0000

    Update Python SDK

--- python/sdk/spapi/configuration.py
+++ python/sdk/spapi/configuration.py
@@ -30,7 +30,7 @@ class Configuration(object):
             return
 
         # Default Base url
-        self.host = "https://sellingpartnerapi-eu.amazon.com"
+        self.host = ""
         # Temp file folder for downloading files
         self.temp_folder_path = None
 
